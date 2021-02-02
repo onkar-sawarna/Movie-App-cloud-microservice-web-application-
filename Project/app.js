ec3a3a0374e08d54146dbcaf387d8183ec0a1cca
onke21 <50941915+onke21@users.noreply.github.com>
2021-02-02 17:40:56 +0530

    working fine

---
+++ Project/app.js
@@ -0,0 +1,54 @@
+const express= require('express');
+const app=express();
+const request=require('request')
+const dotenv = require('dotenv');
+dotenv.config();
+app.set("view engine", "ejs");
+app.use('/public', express.static('public'));
+app.get("/", (req, res)=>{
+    res.render("Home");
+});
+app.get("/aboutme", (req, res)=>{
+    res.render("Aboutme");
+});
+app.get("/result", (req, res)=>{
+    const query = req.query.search;
+    const url = "http://www.omdbapi.com/?apikey=7f4df194&s=" + query;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body)
+            //console.log(data);
+            if(data.Response==='False'){
+                res.send("Movie Not Found");
+            }else{
+                res.render("Result", {data:data});    
+            }
+        }else{
+            res.send('Error');
+        }
+    });
+});
+app.get("/result/:id", (req, res)=>{
+    const url = "http://www.omdbapi.com/?apikey=7f4df194&i=" + req.params.id;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body)
+            //console.log(data);
+            if(data.Response==='False'){
+                res.send("Movie Not Found");
+            }else{
+                //res.send(data);
+                res.render("Info", {movie: data});    
+            }
+        }else{
+            res.send('Error');
+        }
+    });
+});
+
+app.get("*", (req, res)=>{
+    res.send("Some Error");
+});
+app.listen(process.env.PORT, function(){
+    console.log(`Server has started at port ${process.env.PORT}`);
+});
